7e38c998a88589c58501f3c2ae0a0c450ce83781
Laszlo Kishalmi <laszlo_kishalmi@epam.com>
2015-12-08 11:17:35 +0100

    Initial implementation of the Gatling plugin.

---
+++ src/main/groovy/com/github/lkishalmi/gradle/gatling/GatlingExtension.groovy
@@ -0,0 +1,40 @@
+package com.github.lkishalmi.gradle.gatling
+
+import org.gradle.api.Project
+
+/**
+ *
+ * @author Laszlo Kishalmi
+ */
+class GatlingExtension {
+
+    def toolVersion = '2.1.7'
+    def jvmArgs = [
+        '-server', 
+        '-XX:+UseThreadPriorities',
+        '-XX:ThreadPriorityPolicy=42',
+        '-Xms512M',
+        '-Xmx512M',
+        '-Xmn100M',
+        '-XX:+HeapDumpOnOutOfMemoryError',
+        '-XX:+AggressiveOpts',
+        '-XX:+OptimizeStringConcat',
+        '-XX:+UseFastAccessorMethods',
+        '-XX:+UseParNewGC',
+        '-XX:+UseConcMarkSweepGC',
+        '-XX:+CMSParallelRemarkEnabled',
+        '-Djava.net.preferIPv4Stack=true',
+        '-Djava.net.preferIPv6Addresses=false'
+    ]
+    
+    def gatlingSourceSetName = 'gatling'
+    
+    File simulationsDir
+    File dataDir
+    File bodiesDir
+    File reportsDir
+    File confDir
+
+    boolean mute = true
+}
+
---
+++ src/main/groovy/com/github/lkishalmi/gradle/gatling/GatlingPlugin.groovy
@@ -0,0 +1,97 @@
+package com.github.lkishalmi.gradle.gatling
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.plugins.scala.ScalaBasePlugin
+import org.gradle.api.tasks.scala.ScalaCompile
+
+/**
+ *
+ * @author Laszlo Kishalmi
+ */
+class GatlingPlugin implements Plugin<Project> {
+    
+    private final String GATLING_TASK_NAME = 'gatling'
+    private final String GATLING_MAIN_CLASS = 'io.gatling.app.Gatling'
+    private Project project
+    
+    void apply(Project project) {
+        this.project = project
+        
+        project.pluginManager.apply ScalaBasePlugin
+        
+        def gatling = project.extensions.create('gatling', GatlingExtension)
+        
+        gatling.simulationsDir = "$project.projectDir/src/gatling/simulations" as File
+        gatling.dataDir = "$project.projectDir/src/gatling/data" as File
+        gatling.bodiesDir = "$project.projectDir/src/gatling/bodies" as File
+        gatling.reportsDir = "$project.buildDir/reposrts/gatling/" as File
+        gatling.confDir = "$project.projectDir/src/gatling/conf" as File
+        
+        createConfiguration()
+        configureGatlingCompile(gatling)
+        
+        def gatlingTask = project.tasks.create(GATLING_TASK_NAME, Gatling)
+        gatlingTask.description = "Executes all Gatling scenarioes"
+        gatlingTask.group = "Test"
+        
+        
+        project.tasks.addRule('Pattern: gatling<SimulationName>: Executes a named Gatling simulation.') {def taskName ->
+            if (taskName.startsWith(GATLING_TASK_NAME) && !taskName.equals(GATLING_TASK_NAME)) {
+                def simulationName = taskName - GATLING_TASK_NAME
+                project.tasks.create(taskName, Gatling) {
+                    simulation = simulationName
+                }
+            }
+        }
+        
+        project.tasks.withType(Gatling) { Gatling task ->
+            task.dependsOn(project.gatlingCompile)
+            configureGatlingTask(task, gatling)
+        }
+    }
+
+    protected void createConfiguration() {
+        project.configurations.create('gatling').with {
+            visible = false
+            transitive = true
+        }
+    }
+    
+    def configureGatlingCompile(GatlingExtension gatling) {
+        def config = project.configurations['gatling']
+        config.defaultDependencies { dependencies ->
+            dependencies.add(this.project.dependencies.create("io.gatling.highcharts:gatling-charts-highcharts:${gatling.toolVersion}"))
+        }
+        def scalaCompile = project.tasks.create('gatlingCompile', ScalaCompile)
+        scalaCompile.conventionMapping.with {
+            description = { "Compiles Gatling simulations." }
+            source = { project.fileTree(dir: gatling.simulationsDir, includes:['**/*.scala']) }
+            classpath = { config }
+            destinationDir = { project.file("${project.buildDir}/classes/gatling") }
+        }
+        project.gradle.projectsEvaluated {
+            scalaCompile.scalaCompileOptions.incrementalOptions.with {
+                if (!analysisFile) {
+                    analysisFile = new File("$project.buildDir/tmp/scala/compilerAnalysis/${scalaCompile.name}.analysis")
+                }    
+            }
+        }
+    }
+    
+    def configureGatlingTask(Gatling task, GatlingExtension gatling) {
+        task.conventionMapping.with {
+            simulationsDir = { gatling.simulationsDir }
+            dataDir = { gatling.dataDir }
+            bodiesDir = { gatling.bodiesDir }
+            reportsDir = { gatling.reportsDir }
+            confDir = { gatling.confDir }
+            classesDir = { project.gatlingCompile.destinationDir }
+            classpath = { project.configurations['gatling'] + project.files(project.gatlingCompile.destinationDir) }
+            mute = { gatling.mute }
+        }
+    }
+}
+
