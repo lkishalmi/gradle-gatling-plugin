9f3b7a79bbf6547f667524d57dfe24c1392d73e9
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2016-03-17 10:49:25 +0200

    - new, fixed simulation source and data directory structure - multiple simulations can be run during the build - simulations can be filtered from build script - task rule for running single simulation without modifying build script - IDEA project compatibility - Migration to Spock for tests

--- src/main/groovy/com/github/lkishalmi/gradle/gatling/GatlingPlugin.groovy
+++ src/main/groovy/com/github/lkishalmi/gradle/gatling/GatlingPlugin.groovy
@@ -2,95 +2,129 @@ package com.github.lkishalmi.gradle.gatling
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
-import org.gradle.api.tasks.JavaExec
+import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.scala.ScalaBasePlugin
-import org.gradle.api.tasks.scala.ScalaCompile
+import org.gradle.plugins.ide.idea.IdeaPlugin
 
 /**
  *
  * @author Laszlo Kishalmi
  */
 class GatlingPlugin implements Plugin<Project> {
-    
+
     private final String GATLING_TASK_NAME = 'gatling'
+
+    private final String GATLING_TASK_NAME_PREFIX = "$GATLING_TASK_NAME-"
+
     private final String GATLING_MAIN_CLASS = 'io.gatling.app.Gatling'
+
     private Project project
-    
+
     void apply(Project project) {
         this.project = project
-        
+
         project.pluginManager.apply ScalaBasePlugin
-        
-        def gatling = project.extensions.create('gatling', GatlingExtension)
-        
-        gatling.simulationsDir = "$project.projectDir/src/gatling/simulations" as File
-        gatling.dataDir = "$project.projectDir/src/gatling/data" as File
-        gatling.bodiesDir = "$project.projectDir/src/gatling/bodies" as File
-        gatling.reportsDir = "$project.buildDir/reports/gatling/" as File
-        gatling.confDir = "$project.projectDir/src/gatling/conf" as File
-        
-        createConfiguration()
-        configureGatlingCompile(gatling)
-        
-        def gatlingTask = project.tasks.create(GATLING_TASK_NAME, Gatling)
-        gatlingTask.description = "Executes all Gatling scenarioes"
-        gatlingTask.group = "Test"
-        
-        
-        project.tasks.addRule('Pattern: gatling<SimulationName>: Executes a named Gatling simulation.') {def taskName ->
-            if (taskName.startsWith(GATLING_TASK_NAME) && !taskName.equals(GATLING_TASK_NAME)) {
-                def simulationName = taskName - GATLING_TASK_NAME
-                project.tasks.create(taskName, Gatling) {
-                    simulation = simulationName
+        project.pluginManager.apply JavaPlugin
+
+        def gatlingExt = project.extensions.create('gatling', GatlingExtension)
+
+        createConfiguration(gatlingExt)
+        configureGatlingCompile(gatlingExt)
+
+        createGatlingTask(GATLING_TASK_NAME, gatlingExt,
+                project.sourceSets.gatling.allScala.matching(gatlingExt.simulations).collect { File simu ->
+                    (simu.absolutePath - (project.projectDir.absolutePath + "/src/gatling/scala/") - ".scala").replaceAll("/", ".")
+                }
+        )
+
+        project.tasks.addRule('Pattern: gatling-<SimulationClass>: Executes single Gatling simulation.') {
+            def taskName ->
+                if (taskName.startsWith(GATLING_TASK_NAME_PREFIX)) {
+                    createGatlingTask(taskName, gatlingExt, [taskName - GATLING_TASK_NAME_PREFIX])
+                }
+        }
+
+        project.afterEvaluate {
+            def hasIdea = project.plugins.findPlugin(IdeaPlugin)
+            if (hasIdea) {
+                project.idea {
+                    module {
+                        scopes.TEST.plus += [project.configurations.gatlingCompile]
+                    }
+                }
+                project.idea {
+                    module {
+                        project.sourceSets.gatling.scala.srcDirs.each {
+                            testSourceDirs += project.file(it)
+                        }
+                        project.sourceSets.gatling.resources.srcDirs.each {
+                            testSourceDirs += project.file(it)
+                        }
+                    }
                 }
             }
         }
-        
-        project.tasks.withType(Gatling) { Gatling task ->
-            task.dependsOn(project.gatlingCompile)
-            configureGatlingTask(task, gatling)
+    }
+
+    protected void createConfiguration(GatlingExtension gatlingExtension) {
+        project.configurations {
+            gatlingCompile {
+                visible = false
+            }
+            gatlingRuntime {
+                visible = false
+            }
+        }
+
+        project.sourceSets {
+            gatling {
+                scala.srcDirs 'src/gatling/scala'
+                resources.srcDirs 'src/gatling/resources'
+            }
+        }
+
+        project.dependencies {
+            gatlingCompile "io.gatling.highcharts:gatling-charts-highcharts:${gatlingExtension.toolVersion}"
+
+            gatlingCompile project.sourceSets.main.output
+            gatlingCompile project.sourceSets.test.output
         }
     }
 
-    protected void createConfiguration() {
-        project.configurations.create('gatling').with {
-            visible = false
-            transitive = true
-        }
-    }
-    
     def configureGatlingCompile(GatlingExtension gatling) {
-        def config = project.configurations['gatling']
-        config.defaultDependencies { dependencies ->
-            dependencies.add(this.project.dependencies.create("io.gatling.highcharts:gatling-charts-highcharts:${gatling.toolVersion}"))
-        }
-        def scalaCompile = project.tasks.create('gatlingCompile', ScalaCompile)
+        def scalaCompile = project.tasks["compileGatlingScala"]
         scalaCompile.conventionMapping.with {
             description = { "Compiles Gatling simulations." }
-            source = { project.fileTree(dir: gatling.simulationsDir, includes:['**/*.scala']) }
-            classpath = { config }
-            destinationDir = { project.file("${project.buildDir}/classes/gatling") }
         }
         project.gradle.projectsEvaluated {
             scalaCompile.scalaCompileOptions.incrementalOptions.with {
                 if (!analysisFile) {
                     analysisFile = new File("$project.buildDir/tmp/scala/compilerAnalysis/${scalaCompile.name}.analysis")
-                }    
+                }
             }
         }
     }
-    
-    def configureGatlingTask(Gatling task, GatlingExtension gatling) {
-        task.conventionMapping.with {
-            simulationsDir = { gatling.simulationsDir }
-            dataDir = { gatling.dataDir }
-            bodiesDir = { gatling.bodiesDir }
-            reportsDir = { gatling.reportsDir }
-            confDir = { gatling.confDir }
-            classesDir = { project.gatlingCompile.destinationDir }
-            classpath = { project.configurations['gatling'] + project.files(project.gatlingCompile.destinationDir) }
-            mute = { gatling.mute }
+
+    def createGatlingTask(String taskName, GatlingExtension gatlingExt, Collection<String> simulations) {
+        project.tasks.create(name: taskName, dependsOn: project.tasks.gatlingClasses,
+                description: "Execute Gatling simulation", group: "Gatling") << {
+
+            simulations.each { String simu ->
+                project.javaexec {
+                    main = GATLING_MAIN_CLASS
+                    classpath = project.configurations.gatlingRuntime
+                    args "-m"
+                    args "-bf", "${project.sourceSets.gatling.output.classesDir}"
+                    args "-s", simu
+                    args "-df", "${project.sourceSets.gatling.output.resourcesDir}/data"
+                    args "-bdf", "${project.sourceSets.gatling.output.resourcesDir}/bodies"
+                    args "-rf", "${project.reportsDir}/gatling"
+
+                    jvmArgs = gatlingExt.jvmArgs
+
+                    standardInput = System.in
+                }
+            }
         }
     }
 }
