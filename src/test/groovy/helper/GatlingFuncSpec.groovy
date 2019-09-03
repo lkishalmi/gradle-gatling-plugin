642d0cc2f59aab953af1ab8a19c8d0d9499e92aa
Ievgenii Shepeliuk <eshepelyuk@gmail.com>
2019-09-03 21:36:14 +0300

    chore(test): classpath cleanup

--- src/test/groovy/helper/GatlingFuncSpec.groovy
+++ src/test/groovy/helper/GatlingFuncSpec.groovy
@@ -8,15 +8,6 @@ abstract class GatlingFuncSpec extends GatlingSpec {
 
     static def GATLING_HOST_NAME_SYS_PROP = "-Dgatling.hostName=HTTP://COMPUTER-DATABASE.GATLING.IO"
 
-    @Shared
-    List<File> pluginClasspath
-
-    def setupSpec() {
-        def current = getClass().getResource("/").file
-        pluginClasspath = [current.replace("classes/test", "classes/main"),
-                           current.replace("classes/test", "resources/main")].collect { new File(it) }
-    }
-
     File prepareTest(boolean copyFiles = true) {
         createBuildFolder(copyFiles)
         generateBuildScripts()
@@ -25,7 +16,6 @@ abstract class GatlingFuncSpec extends GatlingSpec {
     BuildResult executeGradle(String task) {
         GradleRunner.create().forwardOutput()
             .withProjectDir(testProjectDir.getRoot())
-            .withPluginClasspath(pluginClasspath)
             .withArguments("--stacktrace", GATLING_HOST_NAME_SYS_PROP, task)
             .withDebug(true)
             .build()
@@ -34,7 +24,6 @@ abstract class GatlingFuncSpec extends GatlingSpec {
     BuildResult executeGradle(String task,String gradleVersion) {
         GradleRunner.create().forwardOutput()
             .withProjectDir(testProjectDir.getRoot())
-            .withPluginClasspath(pluginClasspath)
             .withArguments("--stacktrace", GATLING_HOST_NAME_SYS_PROP, task)
             .withDebug(true)
             .withGradleVersion(gradleVersion)
