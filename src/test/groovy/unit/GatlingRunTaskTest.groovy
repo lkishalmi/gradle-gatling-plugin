f7e81746316b61e2b332f7d069499da1364b9274
Ievgenii Shepeliuk <eshepelyuk@newagesol.com>
2019-09-03 18:14:20 +0300

    fix: configurable source of gatling simulations
    Resolves #90

--- src/test/groovy/unit/GatlingRunTaskTest.groovy
+++ src/test/groovy/unit/GatlingRunTaskTest.groovy
@@ -1,6 +1,11 @@
 package unit
 
+
 import helper.GatlingUnitSpec
+
+import static com.github.lkishalmi.gradle.gatling.GatlingPluginExtension.SIMULATIONS_DIR
+import static org.apache.commons.io.FileUtils.copyFileToDirectory
+import static org.apache.commons.io.FileUtils.moveFileToDirectory
 
 class GatlingRunTaskTest extends GatlingUnitSpec {
 
@@ -11,9 +16,9 @@ class GatlingRunTaskTest extends GatlingUnitSpec {
         and:
         gatlingRunSimulations.size() == 2
         and:
-        gatlingRunSimulations.any { it.endsWith("AdvancedSimulationStep03") }
+        "computerdatabase.advanced.AdvancedSimulationStep03" in gatlingRunSimulations
         and:
-        gatlingRunSimulations.any { it.endsWith("BasicSimulation") }
+        "computerdatabase.BasicSimulation" in gatlingRunSimulations
     }
 
     def "should override simulations filter via extension"() {
@@ -27,9 +32,7 @@ class GatlingRunTaskTest extends GatlingUnitSpec {
         def gatlingRunSimulations = gatlingExt.resolveSimulations(gatlingRunTask.simulations)
         gatlingRunSimulations == gatlingExt.resolveSimulations()
         and:
-        gatlingRunSimulations.size() == 1
-        and:
-        gatlingRunSimulations.head().endsWith("AdvancedSimulationStep03")
+        gatlingRunSimulations == ["computerdatabase.advanced.AdvancedSimulationStep03"]
     }
 
     def "should override simulations filter via task properties"() {
@@ -43,7 +46,52 @@ class GatlingRunTaskTest extends GatlingUnitSpec {
         and:
         gatlingRunSimulations.size() == 1
         and:
-        gatlingRunSimulations.head().endsWith("BasicSimulation")
+        gatlingRunSimulations == ["computerdatabase.BasicSimulation"]
+    }
+
+    def "should override simulations dirs via sourceSet"() {
+        given:
+        def overridenSrc = "test/gatling/scala"
+
+        when: 'fake source dirs without simulations'
+        project.sourceSets {
+            gatling {
+                scala.srcDirs = [overridenSrc]
+            }
+        }
+        then:
+        gatlingExt.resolveSimulations().size() == 0
+
+        when:
+        copyFileToDirectory(new File(testProjectDir.root, "${SIMULATIONS_DIR}/computerdatabase/BasicSimulation.scala"),
+            new File(testProjectDir.root, "$overridenSrc/computerdatabase"))
+        then:
+        gatlingExt.resolveSimulations() == ["computerdatabase.BasicSimulation"]
+    }
+
+    def "should extend simulations dirs via sourceSet"() {
+        given:
+        def overridenSrc = "test/gatling/scala"
+
+        when: 'fake source dirs without simulations'
+        project.sourceSets {
+            gatling {
+                scala.srcDir overridenSrc
+            }
+        }
+        then:
+        gatlingExt.resolveSimulations().size() == 2
+
+        when: "temporary hide one simulation"
+        moveFileToDirectory(new File(testProjectDir.root, "${SIMULATIONS_DIR}/computerdatabase/BasicSimulation.scala"),
+            testProjectDir.root, true)
+        then:
+        gatlingExt.resolveSimulations() == ["computerdatabase.advanced.AdvancedSimulationStep03"]
+
+        when:
+        moveFileToDirectory(new File(testProjectDir.root, "BasicSimulation.scala"), new File(testProjectDir.root, "$overridenSrc/computerdatabase"), true)
+        then:
+        gatlingExt.resolveSimulations().size() == 2
     }
 
     def "should use jvmArgs from extension"() {
